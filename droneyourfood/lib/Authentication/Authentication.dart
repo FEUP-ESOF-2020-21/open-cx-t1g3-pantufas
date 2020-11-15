55ad930d272bceb55de59a7883d0a38ab5c5c768
ricardofontao2000 <up201806317@fe.up.pt>
2020-11-15 16:40:43 +0000

    Added appbar to signin and register

--- droneyourfood/lib/Authentication/Authentication.dart
+++ droneyourfood/lib/Authentication/Authentication.dart
@@ -13,100 +13,121 @@ class _SignInState extends State<SignIn> {
   TextEditingController _passwordField = TextEditingController();
   String error = "";
 
+  void navigateToHomeScreen(context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => MyHomePage(title: "Drone your food"),
+      ),
+    );
+  }
+
+  Widget smallSpacer() {
+    return SizedBox(height: MediaQuery.of(context).size.height / 35);
+  }
+
+  void navigateToRegisterScreen(context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => Register()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(
+            "Drone your food",
+            textAlign: TextAlign.left,
+          ),
+        ),
         body: Container(
             child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-          width: MediaQuery.of(context).size.width / 1.3,
-          child: TextFormField(
-            style: TextStyle(color: Colors.white),
-            controller: _emailField,
-            decoration: InputDecoration(
-              hintText: "Email",
-              hintStyle: TextStyle(
-                color: Colors.grey,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width / 1.3,
+              child: TextFormField(
+                style: TextStyle(color: Colors.white),
+                controller: _emailField,
+                decoration: InputDecoration(
+                  hintText: "Email",
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
-        Container(
-          width: MediaQuery.of(context).size.width / 1.3,
-          child: TextFormField(
-            style: TextStyle(color: Colors.white),
-            controller: _passwordField,
-            obscureText: true,
-            decoration: InputDecoration(
-              hintText: "password",
-              hintStyle: TextStyle(
-                color: Colors.grey,
+            Container(
+              width: MediaQuery.of(context).size.width / 1.3,
+              child: TextFormField(
+                style: TextStyle(color: Colors.white),
+                controller: _passwordField,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "password",
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
-        Container(
-            width: MediaQuery.of(context).size.width / 1.2,
-            height: 45,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15.0),
-              color: Colors.black,
+            Container(
+                width: MediaQuery.of(context).size.width / 1.2,
+                height: 45,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15.0),
+                  color: Colors.black,
+                ),
+                child: MaterialButton(
+                    onPressed: () async {
+                      try {
+                        await FirebaseAuth.instance.signInWithEmailAndPassword(
+                            email: _emailField.text,
+                            password: _passwordField.text);
+                        //Only works if the user signs in
+                        navigateToHomeScreen(context);
+                      } on FirebaseAuthException catch (e) {
+                        if (e.code == 'user-not-found') {
+                          setState(() {
+                            error = "User not found";
+                          });
+                          debugPrint('No user found for that email.');
+                        } else if (e.code == 'wrong-password') {
+                          setState(() {
+                            error = "Wrong password";
+                          });
+                          debugPrint('Wrong password provided for that user.');
+                        } else {
+                          setState(() {
+                            error = "Unknown error";
+                          });
+                          debugPrint(e.code);
+                        }
+                      }
+                    },
+                    child: Text("Login"))),
+            smallSpacer(),
+            //Clickable link to register page
+            Container(
+              child: RichText(
+                  text: TextSpan(
+                      text: 'Register',
+                      style: TextStyle(color: Colors.blue),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          navigateToRegisterScreen(context);
+                        })),
             ),
-            child: MaterialButton(
-                onPressed: () async {
-                  try {
-                    await FirebaseAuth.instance.signInWithEmailAndPassword(
-                        email: _emailField.text, password: _passwordField.text);
-                    //Only works if the user signs in
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            MyHomePage(title: "Drone your food"),
-                      ),
-                    );
-                  } on FirebaseAuthException catch (e) {
-                    if (e.code == 'user-not-found') {
-                      setState(() {
-                        error = "User not found";
-                      });
-                      debugPrint('No user found for that email.');
-                    } else if (e.code == 'wrong-password') {
-                      setState(() {
-                        error = "Wrong password";
-                      });
-                      debugPrint('Wrong password provided for that user.');
-                    } else {
-                      setState(() {
-                        error = "Unknown error";
-                      });
-                      debugPrint(e.code);
-                    }
-                  }
-                },
-                child: Text("Login"))),
-        Container(
-          child: RichText(
-              text: TextSpan(
-                  text: 'Register',
-                  style: TextStyle(color: Colors.blue),
-                  recognizer: TapGestureRecognizer()
-                    ..onTap = () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => Register(),
-                        ),
-                      );
-                    })),
-        ),
-        Container(
-          child: Text(error, style: TextStyle(color: Colors.red)),
-        )
-      ],
-    )));
+            //Error displaying after failed signin
+            Container(
+              child: Text(error, style: TextStyle(color: Colors.red)),
+            )
+          ],
+        )));
   }
 }
 
@@ -120,84 +141,100 @@ class _RegisterState extends State<Register> {
   TextEditingController _passwordField = TextEditingController();
   String error = "";
 
+  void navigateToHomeScreen(context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => MyHomePage(title: "Drone your food"),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(
+            "Drone your food",
+            textAlign: TextAlign.left,
+          ),
+        ),
         body: Container(
             child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-          width: MediaQuery.of(context).size.width / 1.3,
-          child: TextFormField(
-            style: TextStyle(color: Colors.white),
-            controller: _emailField,
-            decoration: InputDecoration(
-              hintText: "Email",
-              hintStyle: TextStyle(
-                color: Colors.grey,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width / 1.3,
+              child: TextFormField(
+                style: TextStyle(color: Colors.white),
+                controller: _emailField,
+                decoration: InputDecoration(
+                  hintText: "Email",
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
-        Container(
-          width: MediaQuery.of(context).size.width / 1.3,
-          child: TextFormField(
-            style: TextStyle(color: Colors.white),
-            controller: _passwordField,
-            obscureText: true,
-            decoration: InputDecoration(
-              hintText: "password",
-              hintStyle: TextStyle(
-                color: Colors.grey,
+            Container(
+              width: MediaQuery.of(context).size.width / 1.3,
+              child: TextFormField(
+                style: TextStyle(color: Colors.white),
+                controller: _passwordField,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "password",
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
-        Container(
-            width: MediaQuery.of(context).size.width / 1.2,
-            height: 45,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15.0),
-              color: Colors.black,
-            ),
-            child: MaterialButton(
-                onPressed: () async {
-                  try {
-                    await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                        email: _emailField.text, password: _passwordField.text);
-                    //Only works if the user signs in
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            MyHomePage(title: "Drone your food"),
-                      ),
-                    );
-                  } on FirebaseAuthException catch (e) {
-                    if (e.code == 'weak-password') {
-                      setState(() {
-                        error = 'The password provided is too weak.';
-                      });
-                      debugPrint('The password provided is too weak.');
-                    } else if (e.code == 'email-already-in-use') {
-                      setState(() {
-                        error = 'The account already exists for that email.';
-                      });
-                      debugPrint('The account already exists for that email.');
-                    } else {
-                      setState(() {
-                        error = "Unknown error";
-                      });
-                      debugPrint(e.code);
-                    }
-                  }
-                },
-                child: Text("Register"))),
-        Container(
-          child: Text(error, style: TextStyle(color: Colors.red)),
-        )
-      ],
-    )));
+            Container(
+                width: MediaQuery.of(context).size.width / 1.2,
+                height: 45,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15.0),
+                  color: Colors.black,
+                ),
+                child: MaterialButton(
+                    onPressed: () async {
+                      try {
+                        await FirebaseAuth.instance
+                            .createUserWithEmailAndPassword(
+                                email: _emailField.text,
+                                password: _passwordField.text);
+                        //Only works if the user signs in
+                        navigateToHomeScreen(context);
+                      } on FirebaseAuthException catch (e) {
+                        if (e.code == 'weak-password') {
+                          setState(() {
+                            error = 'The password provided is too weak.';
+                          });
+                          debugPrint('The password provided is too weak.');
+                        } else if (e.code == 'email-already-in-use') {
+                          setState(() {
+                            error =
+                                'The account already exists for that email.';
+                          });
+                          debugPrint(
+                              'The account already exists for that email.');
+                        } else {
+                          setState(() {
+                            error = "Unknown error";
+                          });
+                          debugPrint(e.code);
+                        }
+                      }
+                    },
+                    child: Text("Register"))),
+            //Error displaying after failed signin
+            Container(
+              child: Text(error, style: TextStyle(color: Colors.red)),
+            )
+          ],
+        )));
   }
 }
