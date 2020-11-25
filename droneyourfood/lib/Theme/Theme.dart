6593a5aa581054c1bb90a4588f3f4d0261db7052
JoaoCostaIFG <joaocosta.work@posteo.net>
2020-11-25 08:27:34 +0000

    Add: moved ThemeData to new class.

---
+++ droneyourfood/lib/Theme/Theme.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class MyTheme {
+  final swatch = const MaterialColor(0xFF202124, const <int, Color>{
+    50: const Color(0xFF32674C),
+    100: const Color(0xFFCFD3D8),
+    200: const Color(0xFFABAFB1),
+    300: const Color(0xFF929597),
+    400: const Color(0xFF6D7172),
+    500: const Color(0xFF4E5254),
+    600: const Color(0xFF3c4042),
+    700: const Color(0xFF202124),
+    800: const Color(0xFF1D1D1D),
+    900: const Color(0xFF121212),
+  });
+
+  ThemeData themeData;
+
+  MyTheme() {
+    this.themeData = ThemeData(
+      brightness: Brightness.dark,
+      primaryColor: swatch.shade800,
+      accentColor: swatch.shade800,
+      primarySwatch: swatch,
+      colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: swatch,
+          primaryColorDark: swatch.shade800,
+          accentColor: swatch.shade50,
+          cardColor: swatch.shade500,
+          backgroundColor: swatch.shade800,
+          brightness: Brightness.dark),
+      scaffoldBackgroundColor: swatch.shade900,
+    );
+  }
+}
--- droneyourfood/lib/main.dart
+++ droneyourfood/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:droneyourfood/Theme/Theme.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -11,22 +12,10 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  final swatch = const MaterialColor(0xFF202124, const <int, Color>{
-    50: const Color(0xFF32674C),
-    100: const Color(0xFFCFD3D8),
-    200: const Color(0xFFABAFB1),
-    300: const Color(0xFF929597),
-    400: const Color(0xFF6D7172),
-    500: const Color(0xFF4E5254),
-    600: const Color(0xFF3c4042),
-    700: const Color(0xFF202124),
-    800: const Color(0xFF1D1D1D),
-    900: const Color(0xFF121212),
-  });
-
   @override
   Widget build(BuildContext context) {
     final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+    final MyTheme myTheme = MyTheme();
 
     return FutureBuilder(
       // Initialize FlutterFire:
@@ -36,20 +25,7 @@ class MyApp extends StatelessWidget {
         if (snapshot.connectionState == ConnectionState.done) {
           return MaterialApp(
             title: 'Drone your food',
-            theme: ThemeData(
-              brightness: Brightness.dark,
-              primaryColor: swatch.shade800,
-              accentColor: swatch.shade800,
-              primarySwatch: swatch,
-              colorScheme: ColorScheme.fromSwatch(
-                  primarySwatch: swatch,
-                  primaryColorDark: swatch.shade800,
-                  accentColor: swatch.shade50,
-                  cardColor: swatch.shade500,
-                  backgroundColor: swatch.shade800,
-                  brightness: Brightness.dark),
-              scaffoldBackgroundColor: swatch.shade900,
-            ),
+            theme: myTheme.themeData,
             home: SignIn(),
           );
         }
