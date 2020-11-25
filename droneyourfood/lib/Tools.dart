4470a57b85df58fb9a38f36734ff22512e874acd
JoaoCostaIFG <joaocosta.work@posteo.net>
2020-11-25 00:19:01 +0000

    Fix: checkout button wasn't calling the correct function.
    Update: reorganized some code.

--- droneyourfood/lib/Tools.dart
+++ droneyourfood/lib/Tools.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-import 'package:droneyourfood/Authentication/Profile.dart';
+import 'package:droneyourfood/Profile/Profile.dart';
 import 'package:droneyourfood/Shopping/Shopping.dart';
 
 class Tools {
