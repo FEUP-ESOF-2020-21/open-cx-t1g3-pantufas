d199840a634a412b6441147e5abc5fd3171321f7
JoaoCostaIFG <joaocosta.work@posteo.net>
2020-12-13 19:20:17 +0000

    Fix: removed hardcoded username and password.

--- droneyourfood/lib/Authentication/Signin.dart
+++ droneyourfood/lib/Authentication/Signin.dart
@@ -50,8 +50,7 @@ class _SignInState extends AuthState<SignIn> {
     try {
       UserCredential userCredential = await FirebaseAuth.instance
           .signInWithEmailAndPassword(
-              email: "nachosuwu@protonmail.com", password: "nachosnomnom");
-              // email: emailField.text, password: passwordField.text);
+              email: emailField.text, password: passwordField.text);
 
       if (!userCredential.user.emailVerified) {
         setState(() {
